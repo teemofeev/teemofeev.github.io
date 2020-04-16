f83b8b0f06a655f502f6908fd66a5da6edaa621b
timadevelop <timadevelop@gmail.com>
2020-04-16 14:54:24 +0300

    fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
+set -e
 
-STATUS="$(git status)"
+log() {
+  printf "\n$1\n"
+}
 
-if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+back_to_main() {
+  log "Going back to main branch..." &&
+  git checkout main &&
+  log "Deleting tmp branch..." &&
+  git branch -D tmp-release
+}
+
+cancel() {
+  log "Some error occurred... cancelling the release process"
+  back_to_main
+}
+
+release() {
+  log "Deleting old branch..."
+  git branch -D tmp-release
+  log "Checking out to a new tmp-release branch..." &&
+  git checkout -b tmp-release &&
+  log "Merging with main..." &&
+  git merge --no-edit main &&
+  log "Unignoring dist folder..." &&
+  sed -i '/dist/d' ./.gitignore &&
+  log "Deleting the index.original.html..." &&
+  echo 'index.original.html' >> ./.gitignore &&
+  log "git add..." &&
+  git add . &&
+  log "commit..." &&
+  git commit -m "Edit .gitignore to release" &&
+  log "Deleting old release-to-master split branch..." &&
+  (git branch -D release-to-master &>/dev/null || true) &&
+  log "Subtree split ..." &&
+  git subtree split --prefix dist/apps/website/browser/ -b release-to-master &&
+  log "Pushing to master..." &&
+  git push origin release-to-master:master --force
+}
+
+if [ -z "$(git status --porcelain)" ]
 then
-    sed -i '/dist/d' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git subtree split --prefix dist/apps/website/browser/ -b release-to-master
-    git push origin release-to-master:master --force
-    git reset HEAD~
-    git checkout .gitignore
+  (release && back_to_main) || cancel
 else
-    echo "Need clean working directory to publish"
+  log "Clean working directory to release"
 fi
 
