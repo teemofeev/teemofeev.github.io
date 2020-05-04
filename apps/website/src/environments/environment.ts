567a6d93dd57afddb6b6237797ac85fc421533d3
timadevelop <timadevelop@gmail.com>
2020-05-04 17:59:53 +0300

    New b/w design

--- apps/website/src/environments/environment.ts
+++ apps/website/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  email: 'timadevelop@gmail.com'
 };
 
 /*
